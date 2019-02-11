046e6899d342c3daa80533e95992f5b48b2f34e4
Benjamin Wunn <b.wunn@webweit.de>
2019-02-11 16:30:14 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,47 @@
 "use strict";
 
 global.$ = {
-  path: {
-    task: require("./gulp/path/tasks.js")
-  },
-  gulp: require("gulp"),
-  browserSync: require("browser-sync").create(),
-  del: require("del")
+    path: {
+        task: require('./gulp/path/tasks.js')
+    },
+    gulp: require('gulp'),
+    browserSync: require('browser-sync').create(),
+    del: require('del')
 };
 
-$.path.task.forEach(function(taskPath) {
-  require(taskPath)();
+$.path.task.forEach(function (taskPath) {
+    require(taskPath)();
 });
 
-$.gulp.task(
-  "dev",
-  $.gulp.series(
-    "clean",
+$.gulp.task('dev', $.gulp.series(
+    'clean',
     $.gulp.parallel(
-      "pug",
-      "fonts",
-      "styles:dev",
-      "img:dev",
-      "libsJS:dev",
-      "js:dev",
-      "svg"
+        'pug',
+        'fonts',
+        'styles:dev',
+        'img:dev',
+        'libsJS:dev',
+        'js:dev',
+        'svg'
     )
-  )
-);
+));
 
-$.gulp.task(
-  "build",
-  $.gulp.series(
-    "clean",
+$.gulp.task('build', $.gulp.series(
+    'clean',
     $.gulp.parallel(
-      "pug",
-      "fonts",
-      "styles:build-min",
-      "img:build",
-      "libsJS:build",
-      "js:build-min",
-      "svg"
+        'pug',
+        'fonts',
+        'styles:build-min',
+        'img:build',
+        'libsJS:build',
+        'js:build-min',
+        'svg'
     )
-  )
-);
-$.gulp.task("default", $.gulp.series("dev", $.gulp.parallel("watch", "serve")));
+));
+$.gulp.task('default', $.gulp.series(
+    'dev',
+    $.gulp.parallel(
+        'watch',
+        'serve'
+    )
+));
